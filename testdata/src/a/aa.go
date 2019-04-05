77457afc2da35c81b303186c294f19ad30b27fc4
izumix03 <takahiro.izumikawa@gmail.com>
2019-04-05 23:47:21 +0900

    add pattern

---
+++ testdata/src/a/aa.go
@@ -0,0 +1,9 @@
+package a
+
+import "fmt"
+
+type Fuga struct {
+	Test string
+	Name int
+	fmt.Stringer
+}
--- testdata/src/a/b.go
+++ testdata/src/a/b.go
@@ -7,6 +7,21 @@ func main() {
 	println(a)
 	println(Hoge{ // want "uninitialised field found: Name" "uninitialised field found: Test"
 	})
+
+	c := Fuga{
+		"",
+		aaa(),
+		nil,
+	}
+	println(c)
+	c = Fuga{ // want "uninitialised field found: Stringer" "uninitialised field found: Test"
+		Name: aaa(),
+	}
+	println(c)
+}
+
+func aaa() int {
+	return 1
 }
 
 type Hoge struct {
